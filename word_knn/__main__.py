d3b212651c81f496d5954694b95b4d84a452cdeb
Romain Beaumont <romain.rom1@gmail.com>
2019-08-26 00:56:09 +0200

    add count option in cli

--- word_knn/__main__.py
+++ word_knn/__main__.py
@@ -9,6 +9,7 @@ home = str(Path.home())
 def main():
     parser = argparse.ArgumentParser(description='Find closest words.')
     parser.add_argument('--word', help='word', type=str, default="cat")
+    parser.add_argument('--count', help='number of nearest neighboors', type=int, default=10)
     parser.add_argument('--root_embeddings_dir', help='dir to save embeddings', type=str, default=home + "/embeddings")
     parser.add_argument('--embeddings_id', help='word embeddings id from http://vectors.nlpl.eu/repository/', type=str,
                         default="0")
@@ -17,7 +18,7 @@ def main():
     args = parser.parse_args()
 
     closestWords = from_nlpl(args.root_embeddings_dir, args.embeddings_id, args.save_zip)
-    print(closestWords.closest_words(args.word, 10))
+    print(closestWords.closest_words(args.word, args.count))
 
 
 if __name__ == '__main__':
